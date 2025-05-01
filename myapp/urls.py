78e6b3f9bce02e278d529fc929d1f7a62f6eac05
M0rty <133975972+ItsM0rty@users.noreply.github.com>
2025-05-01 21:29:33 +0545

    home page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('about/', views.about, name='about'),
+    # path('contact/', views.contact, name='contact'),
+    # path('sales/', views.sales, name='sales'),
+    # path('login/', views.login_view, name='login'),
 ]
