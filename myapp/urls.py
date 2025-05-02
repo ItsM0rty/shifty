e2228273457e4cf8f130f8fee18250163c8646b7
itbcharles <cafreidenreich@gmail.com>
2025-05-01 22:54:46 -0700

    Updated home page

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from . import views 
+
+app_name = 'myapp' 
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('about/', views.about, name='about'),
-    # path('contact/', views.contact, name='contact'),
-    # path('sales/', views.sales, name='sales'),
-    # path('login/', views.login_view, name='login'),
+
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
+    path('sales/', views.sales_view, name='sales'),
+    path('login/', views.login_view, name='login'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'myapp/index.html')
+
+def about_view(request):
+    return render(request, 'myapp/about.html', {})
+
+def contact_view(request):
+    return render(request, 'myapp/contact.html', {})
+
+def sales_view(request):
+    return render(request, 'myapp/sales.html', {})
+
+def login_view(request):
+    return render(request, 'myapp/login.html', {})
