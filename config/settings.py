4afbfea0e01e1fb63a513ea8e99812188397c393
itbcharles <cafreidenreich@gmail.com>
2025-06-14 15:41:56 -0700

    Final fix?

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 # Static files configuration
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'myapp/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
